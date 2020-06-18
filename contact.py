85215bca58d18ac87a3ea61860601c93906d69b0
Andrei Chaikin <st_andrew@list.ru>
2020-06-18 17:13:15 +0300

    New test for task 3 implemented

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+
+    def __init__(self, last_name, first_name, middle_name, nickname, company, address, birthday_day, email, mobile, birthday_month, birthday_year):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.birthday_day = birthday_day
+        self.email = email
+        self.mobile = mobile
+        self.birthday_month = birthday_month
+        self.birthday_year = birthday_year
