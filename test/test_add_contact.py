3f0e554ef04f6a1e91b73006598d796a5e98c697
Andrei Chaikin <st_andrew@list.ru>
2020-06-23 19:51:22 +0300

    Created packages for different responsibility areas

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from contact import Contact
-from application import Application
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
