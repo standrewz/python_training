029912c4baf7cb8860e1b668b6a634ec4823f252
Andrei Chaikin <st_andrew@list.ru>
2020-08-14 14:31:35 +0300

    Fixed test to check contact data on main page

--- test/test_contact_data.py
+++ test/test_contact_data.py
@@ -31,7 +31,18 @@ def merge_emails_like_on_home_page(contact):
 def test_contact_data_on_home_page(app, db):
     contacts_from_home_page = app.contact.get_contact_list()
     contacts_from_db = db.get_contact_list()
-    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
+    for db_contact in contacts_from_db:
+        db_contact.all_phones_from_home_page = merge_phones_like_on_home_page(db_contact)
+        db_contact.all_emails_from_home_page = merge_emails_like_on_home_page(db_contact)
+    contacts_from_db = sorted(contacts_from_db, key=Contact.id_or_max)
+    contacts_from_home_page = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    # Basic check
+    assert contacts_from_home_page == contacts_from_db
+    # Extended checks
+    for index in range(len(contacts_from_db)):
+        assert clear(contacts_from_db[index].address) == clear(contacts_from_home_page[index].address)
+        assert contacts_from_db[index].all_phones_from_home_page == contacts_from_home_page[index].all_phones_from_home_page
+        assert contacts_from_db[index].all_emails_from_home_page == contacts_from_home_page[index].all_emails_from_home_page
 
 def test_contact_data_on_edit_page(app, db):
     contacts_from_db = db.get_contact_list()
@@ -55,6 +66,12 @@ def verify_contact_data(expected_contact, actual_contact):
     assert expected_contact.email2 == actual_contact.email2
     assert expected_contact.email3 == actual_contact.email3
     verify_contact_phones(expected_contact, actual_contact)
+
+def verify_contact_phones(expected_contact, actual_contact):
+    assert expected_contact.homephone == actual_contact.homephone
+    assert expected_contact.mobile == actual_contact.mobile
+    assert expected_contact.workphone == actual_contact.workphone
+    assert expected_contact.secondaryphone == actual_contact.secondaryphone
 
 def verify_contact_phones(expected_contact, actual_contact):
     assert expected_contact.homephone == actual_contact.homephone
